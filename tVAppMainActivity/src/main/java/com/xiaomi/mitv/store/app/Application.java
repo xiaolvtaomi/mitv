e9e8c8b2183b29812b195b815777b575fdc2f6ac
wanzhenyu <48138234@qq.com>
2017-09-20 14:37:10 +0800

    服务面对面代码上传

---
+++ tVAppMainActivity/src/main/java/com/xiaomi/mitv/store/app/Application.java
@@ -0,0 +1,20 @@
+package com.xiaomi.mitv.store.app;
+
+import yealink.MainApplication;
+
+/**
+ * MainApplication
+ * 
+ * @author liurs
+ *
+ */
+public class Application extends MainApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    
+
+}
