9b546d926691d6c3b56ec964e43625fa85eaba84
xiaolvtaomi <23185954@qq.com>
2017-09-21 14:24:55 +0800

    MyApplication 即成 MainApplication

--- tVMetroMainActivity/src/main/java/com/app/MyApplication.java
+++ tVMetroMainActivity/src/main/java/com/app/MyApplication.java
@@ -4,11 +4,14 @@ import android.app.Application;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.yealink.MainApplication;
+import com.yealink.NotifyService;
+import com.yealink.lib.common.AbstractYealinkApplication;
 
 /**
  * Created by Lenovo on 2017/5/11.
  */
-public class MyApplication extends Application {
+public class MyApplication extends MainApplication {
     private static RequestQueue mQueue;
     // ceshi
     @Override
