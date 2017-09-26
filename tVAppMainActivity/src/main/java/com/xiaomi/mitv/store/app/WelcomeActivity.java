c9d77fd3c7819adeadcacb527353085fb7ea8d68
wangzhiwei <1479423143@qq.com>
2017-09-26 10:30:07 +0800

    修改欢迎界面

--- tVAppMainActivity/src/main/java/com/xiaomi/mitv/store/app/WelcomeActivity.java
+++ tVAppMainActivity/src/main/java/com/xiaomi/mitv/store/app/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.xiaomi.mitv.store.app;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,9 +12,11 @@ import android.widget.RelativeLayout;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.VolleyHelper;
-import com.tv.ui.metro.sampleapp.R;
 
-public class WelcomeActivity extends AppMainActivity {
+import com.tv.ui.metro.sampleapp.R;
+import com.tv.ui.metro.utils.ViewUtils;
+
+public class WelcomeActivity extends Activity {
     private ImageLoader mImageLoader;
     public String bg;
     RelativeLayout welcome_bg;
@@ -34,15 +37,23 @@ public class WelcomeActivity extends AppMainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        init();
-        SharedPreferences sp = getSharedPreferences("loginUser", Context.MODE_PRIVATE);
+      //  init();
+       // SharedPreferences sp = getSharedPreferences("loginUser", Context.MODE_PRIVATE);
         //取得欢迎界面背景
-        bg = sp.getString("user_bg", "");
-        mImageLoader.get(bg, ImageLoader.getCommonViewImageListener(welcome_bg, R.drawable.bg, R.drawable.bg));
+       // bg = sp.getString("user_bg", "");
+        //mImageLoader.get(bg, ImageLoader.getCommonViewImageListener(welcome_bg, R.drawable.bg, R.drawable.bg));
         handler.sendEmptyMessageDelayed(1,1500 );
     }
-    public void  init(){
-        mImageLoader = VolleyHelper.getInstance(getApplicationContext().getApplicationContext()).getImageLoader();
-        welcome_bg= (RelativeLayout) findViewById(R.id.welcome_bg);
+//    public void  init(){
+//        mImageLoader = VolleyHelper.getInstance(getApplicationContext().getApplicationContext()).getImageLoader();
+//        welcome_bg= (RelativeLayout) findViewById(R.id.welcome_bg);
+//    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ViewUtils.unbindDrawables(findViewById(R.id.welcome_bg));
+
+
     }
+
 }
